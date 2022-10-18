53ece566e5097da04aedb8a5b9528f1f0fb34294
Gaurav Pratap Singh <gs492979@gmail.com>
2022-10-19 00:06:42 +0530

    Jquery library used

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 function submitValue() {
   const emailValue = document.getElementById("email").value;
-  let EmailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
-    emailValue
-  );
-  if (!EmailValidation) {
-    document.querySelector(".error-msg").classList.remove("hidden");
-    document.querySelector(".error-img").classList.remove("hidden");
-    document.querySelector("#email").style.border = "2px solid red";
-    document.querySelector(".btn2").style.bottom = "33.5px";
-  } else {
-    document.querySelector(".error-msg").classList.add("hidden");
-    document.querySelector(".error-img").classList.add("hidden");
-    document.querySelector("#email").style.border = "2px solid lime";
-    document.querySelector(".btn2").style.bottom = "16.5px";
+  let EmailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailValue);
+
+
+  if(!EmailValidation) {
+    $(".error-msg").removeClass("hidden");
+    $(".error-img").removeClass("hidden");
+    $("#email").css("border", "2px solid red");
+    $(".btn2").css("bottom", "34.5px");
+  } 
+  else {
+    $(".error-msg").addClass("hidden");
+    $(".error-img").addClass("hidden");
+    $("#email").css("border", "2px solid lime");
+    $(".btn2").css("bottom", "17.5px");
   }
 }
